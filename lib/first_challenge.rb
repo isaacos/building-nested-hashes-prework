4be458a4af15d5bdc2c4a7dc58b8aeeb723dff2e
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-19 18:21:12 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,11 +1,16 @@
 def first_challenge
    epic_tragedy = {
      montague: {
-       patriarch:{}
-       matriarch: {}
-       hero: {}
+       patriarch:{},
+       matriarch: {},
+       hero: {},
        hero_friends: {}
      },
-     capulet: {}
+     capulet: {
+       patriarch:{},
+       matriarch: {},
+       hero: {},
+       hero_friends: {}
+     }
    }
 end
